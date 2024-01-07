6c988fa68c465992a5e65e502a82365c333ad662
Shire <im@ixu.me>
2024-01-07 19:46:48 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,47 +1,28 @@
 <?php
 date_default_timezone_set('Asia/Shanghai');
-//判断是否随机调用
-if ($_GET['rand']==='true') {
-  $gettime = rand(-1,7);
-}else{
-//若不为随机调用则判断是否指定日期
-  $gettimebase = $_GET['day'];
-  if (empty($gettimebase)) {
-    $gettime = 0;
-  }else{
-    $gettime = $gettimebase;
-  }
-}
-//判断是否指定格式
-$getformatbase = $_GET['format'];
-if (empty($getformatbase)) {
-  $getformat = "jpg";
-}else{
-  $getformat = $getformatbase;
-}
-//获取Bing Json信息
+
+$rand = $_GET['rand'] ?? 'false';
+$gettime = $rand === 'true' ? rand(-1,7) : ($_GET['day'] ?? 0);
+
+$getformat = $_GET['format'] ?? "jpg";
+
 $json_string = file_get_contents('https://cn.bing.com/HPImageArchive.aspx?format=js&idx='.$gettime.'&n=1');
-//转换为PHP数组
 $data = json_decode($json_string);
-//提取基础url
+
 $imgurlbase = "https://cn.bing.com".$data->{"images"}[0]->{"urlbase"};
-//判断是否指定图片大小
-$imgsizebase = $_GET['size'];
-if (empty($imgsizebase)){
-  $imgsize = "1920x1080";
-}else{
-  $imgsize = $imgsizebase;
-}
-//建立完整url
+
+$imgsize = $_GET['size'] ?? "1920x1080";
+
 $imgurl = $imgurlbase."_".$imgsize.".".$getformat."";
-//获取其他信息
+
 $imgtime = $data->{"images"}[0]->{"startdate"};
 $imgtitle = $data->{"images"}[0]->{"copyright"};
 $imglink = $data->{"images"}[0]->{"copyrightlink"};
-//判断是否只获取图片信息
-if ($_GET['info']==='true') {
+
+$info = $_GET['info'] ?? 'false';
+if ($info === 'true') {
   echo "{title:".$imgtitle.",url:".$imgurl.",link:".$imglink.",time:".$imgtime."}";
 }else{
-  //若不是则跳转url
   header("Location: $imgurl");
 }
+?>
