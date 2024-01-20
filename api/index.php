9dae2398d22f1f3ae4b7d6531eaad35b5074923f
Shire <im@ixu.me>
2024-01-20 15:42:21 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,12 +6,39 @@ $gettime = $rand === 'true' ? rand(-1,7) : ($_GET['day'] ?? 0);
 
 $getformat = $_GET['format'] ?? "jpg";
 
-$json_string = file_get_contents('https://cn.bing.com/HPImageArchive.aspx?format=js&idx='.$gettime.'&n=1');
+// List of allowed sizes
+$allowed_sizes = [
+  "3840x2160",
+  "4596x2583",
+  "1920x1080",
+  "1366x768",
+  "1280x768",
+  "1024x768",
+  "800x600",
+  "800x480",
+  "768x1280",
+  "720x1280",
+  "640x480",
+  "480x800",
+  "400x240",
+  "320x240",
+  "240x320"
+];
+
+$imgsize = $_GET['size'] ?? "3840x2160";
+
+// Check if the provided size is allowed
+if (!in_array($imgsize, $allowed_sizes)) {
+  $imgsize = "3840x2160";
+}
+
+list($uhdwidth, $uhdheight) = explode('x', $imgsize);
+
+$json_string = file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&idx='.$gettime.'&n=1&uhd=1&uhdwidth='.$uhdwidth.'&uhdheight='.$uhdheight);
 $data = json_decode($json_string);
 
-$imgurlbase = "https://cn.bing.com".$data->{"images"}[0]->{"urlbase"};
-
-$imgsize = $_GET['size'] ?? "1920x1080";
+// $imgurlbase = "https://cn.bing.com".$data->{"images"}[0]->{"urlbase"};
+$imgurlbase = "https://www.bing.com".$data->{"images"}[0]->{"url"};
 
 $imgurl = $imgurlbase."_".$imgsize.".".$getformat."";
 
@@ -23,6 +50,12 @@ $info = $_GET['info'] ?? 'false';
 if ($info === 'true') {
   echo "{title:".$imgtitle.",url:".$imgurl.",link:".$imglink.",time:".$imgtime."}";
 }else{
+//   $imgData = file_get_contents($imgurl);
+//   header('Content-type: image/jpeg');
+//   echo $imgData;
+// }
+// ?>
+
   header("Location: $imgurl");
 }
 ?>
